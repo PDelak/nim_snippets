a00b2228bd68963a336a17c18c1fe19fc280b463
Delewski <przemyslaw.delewski@gmail.com>
2016-12-05 20:34:14 +0100

    set api update (erase added)

--- set.nim
+++ set.nim
@@ -9,9 +9,12 @@ proc makeSet*[T](node:SNode[T]):Set[T] = makeList(node)
 
 proc insert*[T](set:Set[T], data:T):Set[T] = pushFront(set, data)
 
+proc erase*[T](set:Set[T], data:T):Set[T] = remove(set, data)
+
 proc traverseSet*[T](set:Set[T], p:proc(node:SNode[T])) = traverse(set, p)
 
 when isMainModule:
-    var set = makeSet[int](nil).insert(10).insert(20)
+    var set_1 = makeSet[int](nil).insert(10).insert(20).insert(30)
+    var set_2 = set_1.erase(10)
     var visitor = proc(n : SNode[int]) = echo n.data
-    traverseSet(set, visitor)
+    traverseSet(set_2, visitor)
