c9748d42c2748b9f07f24d2375ab2ac23dd6e4ad
Delewski <przemyslaw.delewski@gmail.com>
2016-12-06 23:57:47 +0100

    mutable insert implemented

--- tree.nim
+++ tree.nim
@@ -6,8 +6,45 @@ type TreeNode[T] = ref object
   right:TreeNode[T]
 
 
+type Tree[T] = ref object
+  root : TreeNode[T]
+
 proc makeNode[T](data:T, left:TreeNode[T], right:TreeNode[T]) : TreeNode[T]
     = TreeNode[T](data:data, left:left, right:right)
+
+proc makeTree[T]() : Tree[T]=
+    Tree[T](root:nil)
+
+proc path[T](node:TreeNode[T], data:T):system.seq[TreeNode[T]] =
+    var stack:system.seq[TreeNode[T]] = @[]
+    var current = node
+    while(current != nil):
+        stack.add(current)
+        if(data < current.data)    : current = current.left
+        elif(data > current.data)  : current = current.right
+        elif(data == current.data) : break
+    result = stack
+
+ 
+proc insert[T](node:TreeNode[T], data:T):TreeNode[T]=
+    var current = node
+    if(current == nil): result = makeNode[T](data, nil, nil)
+    else:
+        var parent = current
+        while(current != nil):
+            parent = current
+            if(data < current.data)    : current = current.left
+            elif(data > current.data)  : current = current.right
+            elif(data == current.data) : break
+        if(data < parent.data):
+            parent.left = makeNode[T](data, nil, nil)
+        else:
+            parent.right = makeNode[T](data, nil, nil)
+        result = node
+
+proc insert[T](tree:Tree[T], data:T)=
+    tree.root = insert(tree.root, data)
+
 
 proc VisitInOrder[T](node:TreeNode[T]) = 
     if(node != nil):
@@ -18,30 +55,12 @@ proc VisitInOrder[T](node:TreeNode[T]) =
 
 
 proc find[T](node:TreeNode[T], data:T):TreeNode[T] =
-    var stack:DoublyLinkedList[T]
     var current = node
     while(current != nil):
-        if(data < current.data):
-            current = current.left
-        elif(data > current.data):
-            current = current.right
-        elif(data == current.data):
-            break
+        if(data < current.data)    : current = current.left
+        elif(data > current.data)  : current = current.right
+        elif(data == current.data) : break
     result = current
-
-proc path[T](node:TreeNode[T], data:T):DoublyLinkedList[TreeNode[T]] =
-    var stack:DoublyLinkedList[TreeNode[T]]
-    var current = node
-    while(current != nil):
-        stack.prepend(current)
-        if(data < current.data):
-            current = current.left
-        elif(data > current.data):
-            current = current.right
-        elif(data == current.data):
-            break
-    result = stack
-
 
 
 var left = makeNode(2, nil, nil)
@@ -54,8 +73,16 @@ var n = find(root, 2)
 
 if n != nil: echo n.data
 
-echo "path:"
+echo "path:" & ":"
 
 var p = path(root,10)
 
 for e in p : echo e.data
+
+var tree = makeTree[int]()
+tree.insert(10)
+tree.insert(20)
+tree.insert(30)
+echo "tree.visit"
+
+VisitInOrder(tree.root)
