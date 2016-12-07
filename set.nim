cdd99974e041dcd2a6e4ef0758a55f39e87c0d01
Delewski <przemyslaw.delewski@gmail.com>
2016-12-07 21:43:47 +0100

    set implementation in terms of list fixes

--- set.nim
+++ set.nim
@@ -1,20 +1,20 @@
 # this module implements set in terms of list
-import list
+import list 
 
 type Set*[T] = List[T]
 
-type SNode*[T] = Node[T]
+type Node*[T] = list.Node[T]
 
-proc makeSet*[T](node:SNode[T]):Set[T] = makeList(node)
+proc makeSet*[T](node:Node[T]):Set[T] = makeList(node)
 
 proc insert*[T](set:Set[T], data:T):Set[T] = pushFront(set, data)
 
-proc erase*[T](set:Set[T], data:T):Set[T] = remove(set, data)
+proc remove*[T](set:Set[T], data:T):Set[T] = list.remove(set, data)
 
-proc traverseSet*[T](set:Set[T], p:proc(node:SNode[T])) = traverse(set, p)
+proc traverse*[T](set:Set[T], p:proc(node:Node[T])) = list.traverse(set, p)
 
 when isMainModule:
     var set_1 = makeSet[int](nil).insert(10).insert(20).insert(30)
-    var set_2 = set_1.erase(10)
-    var visitor = proc(n : SNode[int]) = echo n.data
-    traverseSet(set_2, visitor)
+    var set_2 = set_1.remove(10)
+    var visitor = proc(n : Node[int]) = echo n.data
+    traverse(set_2, visitor)
